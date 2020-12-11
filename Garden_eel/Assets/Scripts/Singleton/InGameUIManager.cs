272f8b43600ee77d9b3f2ea321ff4b4e9a2f11f6
Chanmob <chanmob@gambridzy.com>
2020-12-11 22:32:39 +0900

    UI 작업

--- Garden_eel/Assets/Scripts/Singleton/InGameUIManager.cs
+++ Garden_eel/Assets/Scripts/Singleton/InGameUIManager.cs
@@ -5,5 +5,4 @@ using UnityEngine;
 public class InGameUIManager : Singleton<InGameUIManager>
 {
     public UI_InGameMainUI ui_InGameMainUI;
-
 }
--- Garden_eel/Assets/Scripts/UI/UI_InGameMainUI.cs
+++ Garden_eel/Assets/Scripts/UI/UI_InGameMainUI.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_InGameMainUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Slider _slider_Progress;
+
+    [SerializeField]
+    private Text _text_Score;
+    [SerializeField]
+    private Text _text_Time;
+
+    public void SetTime(string time)
     {
-        
+        //TODO : time이 string이 아닐 수 있음
+        _text_Time.text = time;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetScore(int score)
     {
-        
+        _text_Score.text = score.ToString();
+    }
+
+    public void SetProgress(float value)
+    {
+        _slider_Progress.value = value;
     }
 }
