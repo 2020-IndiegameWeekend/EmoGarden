989042348c856d5d9faf693305af6f90c692924a
sonohoshi <yd111544@gmail.com>
2020-12-12 04:34:51 +0900

    Add readonly size&length array

--- Garden_eel/Assets/Scripts/PlayerObject/GardenEelHead.cs
+++ Garden_eel/Assets/Scripts/PlayerObject/GardenEelHead.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class GardenEelHead : GardenEelBody
 {
     public float lengthOfBody;
-    public Transform tail;
+    public GameObject body;
+    
+    private Transform _tail;
+    private List<GardenEelBody> _bodyList;
+
+    private const float StartSize = 1f;
+    private const float StartLength = 10f;
+
+    private static readonly float[] LengthArray = new[] {1f, 1.9f, 3.3f, 4.3f, 5f};
+    private static readonly float[] SizeArray = new[] {1f, 1.4f, 2f, 2.6f, 3.2f};
+    
+    void Start()
+    {
+        
+    }
 
     protected override bool MoveToTarget(Vector3 target)
     {
-        if ((tail.position - transform.position).magnitude < lengthOfBody)
+        if ((_tail.position - transform.position).magnitude < lengthOfBody)
         {
             return base.MoveToTarget(target);
         }
