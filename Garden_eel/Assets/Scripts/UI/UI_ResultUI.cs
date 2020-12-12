afc85bf9cf07c97b64df08f8fbe2bc0ede7a6b82
Chanmob <chanmob@gambridzy.com>
2020-12-12 12:10:36 +0900

    결과창 UI 스크립트 추가

---
+++ Garden_eel/Assets/Scripts/UI/UI_ResultUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ResultUI : MonoBehaviour
+{
+    private IEnumerator CountingCoroutine(Text _text, float _min, float _max, float _time)
+    {
+        float offset = (_max - _min) / _time;
+
+        while (_min < _max)
+        {
+            _min += offset * Time.deltaTime;
+            _text.text = ((int)_min).ToString();
+            yield return null;
+        }
+
+        _min = _max;
+        _text.text = ((int)_min).ToString();
+    }
+}
