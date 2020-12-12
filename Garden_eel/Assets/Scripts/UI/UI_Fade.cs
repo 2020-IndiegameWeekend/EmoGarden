d8f6f9b947ce7c0f53dc11dce356523b9c750488
Chanmob <chanmob@gambridzy.com>
2020-12-12 14:56:41 +0900

    fade 추가

---
+++ Garden_eel/Assets/Scripts/UI/UI_Fade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Fade : MonoBehaviour
+{
+    private Image _image_Fade;
+
+    private Color _startColor;
+    private Color _targetColor;
+
+    private float _fadeTime;
+
+    private IEnumerator UpdateColorCoroutine()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            _image_Fade.color = Color.Lerp(_startColor, _targetColor, t);
+            t += Time.deltaTime / _fadeTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        _image_Fade.color = _targetColor;
+        _image_Fade.gameObject.SetActive(false);
+    }
+}
