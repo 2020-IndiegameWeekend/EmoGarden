cc905621d212f29694981823456e1743158ec142
Chanmob <chanmob@gambridzy.com>
2020-12-13 01:24:02 +0900

    인게임 수정

--- Garden_eel/Assets/Scripts/DecoFish.cs
+++ Garden_eel/Assets/Scripts/DecoFish.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class DecoFish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float distance;
+    [SerializeField]
+    private float _moveSpeed;
+    [SerializeField]
+    private float _moveRange;
+
+    [SerializeField]
+    private bool _isRightMove;
+
+    private float checkDistance;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
     {
-        
+        checkDistance = _moveRange * distance;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        StartCoroutine(MoveCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator MoveCoroutine()
     {
-        
+        while (true)
+        {
+            Vector2 destination = (Vector2)transform.position;
+
+            if (_isRightMove)
+            {
+                destination.x -= _moveRange;
+                _spriteRenderer.flipX = false;
+            }
+            else
+            {
+                destination.x += _moveRange;
+                _spriteRenderer.flipX = true;
+            }
+
+            while (Vector2.Distance(transform.position, destination) >= checkDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, destination, _moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+
+            _isRightMove = !_isRightMove;
+        }
     }
 }
